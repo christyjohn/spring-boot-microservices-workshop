bb03162229fe5b0cfa29918c260debe615e9cbba
Christy John <christyjohn.crz@gmail.com>
2021-05-05 11:59:15 +0530

    Init

---
+++ movie-data-service/src/test/java/com/christy/moviedataservice/MovieDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christy.moviedataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
